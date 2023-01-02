257be8d85a1abfe2a98b604af4b1df0782e4151f
xeos <syl9456@live.com>
2023-01-02 19:20:46 +0100

    nouveau commit

--- src/app/shared/services/assignments.service.ts
+++ src/app/shared/services/assignments.service.ts
@@ -25,7 +25,7 @@ export class AssignmentsService {
   }
 
   //url = "http://localhost:8010/api_assignment/assignments"
-  url = 'https://api-assignment-heroku.herokuapp.com/api_assignment/assignments/'
+  url = 'https://api-assignment-heroku.herokuapp.com/api_assignment/assignments'
 
 
   // récupération de la liste des assignments
@@ -79,7 +79,7 @@ export class AssignmentsService {
 
   // mise à jour d'un assignment par son id
   updateAssignment(assignment:Assignment): Observable<any> { 
-    return this.http.put<Assignment>(this.url,assignment, this.httpOptions);
+    return this.http.put<Assignment>(this.url, assignment, this.httpOptions);
   }
 
   // pour peupler la base de données avec les assignments du tableau bdInitialAssignments
